884308c05cfbe5f0332ac7e3e367a9496e339daf
Mykola Korsikov <korsikov@ukr.net>
2021-12-29 18:30:14 +0100

    added validation

--- HomeWork_Lesson9.js
+++ HomeWork_Lesson9.js
@@ -1,25 +1,29 @@
 //Introduce strict rule for one of the parameters in the class
-
+"use strict";
 class Movie {
     constructor(title, IMDbRating, duration) {
         this.title = title;
         this.duration = duration;
-        if (isNaN(IMDbRating)) {
-            return console.log('Enter rating as a number (e.g. 9.5):')
-        } else if (parseFloat(IMDbRating) <0 || parseFloat(IMDbRating)> 10) {
+
+        if (isNaN(IMDbRating) && (parseFloat(IMDbRating) <0 || parseFloat(IMDbRating)> 10)) {
             return console.log('Enter rating which is between 0 and 10.00:');
-        } else {
+        } else{
             this.IMDbRating = parseFloat(IMDbRating);
         }
     }
-};
+}
 
 //Creating new object
-const item1 = new Movie('Titanic', '0.00', "3:14");
+const item1 = new Movie('Titanic', '7', "3:14");
 console.log(item1);
 
 //TDD
-if (item1.IMDbRating >= 10 || item1.IMDbRating <= 0 || isNaN(item1.IMDbRating)){
+
+//Assigning wrong attribute directly to object parameter
+item1.IMDbRating = '9';
+
+//Validation
+console.log(item1);if (item1.IMDbRating >= 10 || item1.IMDbRating <= 0 || isNaN(item1.IMDbRating)){
     console.log('Validation failed.');
 } else{
     console.log('Validation passed.');
